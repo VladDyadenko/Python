66e851006618a849960c62c76714d6bef31f84e5
Vladimir Dyadenko <Stilmagroup@gmail.com>
2024-02-11 21:34:34 +0200

    first commit

---
+++ hello.py
@@ -0,0 +1,25 @@
+# nam=input('Who are you?')
+# print("Welkom", nam)
+
+# inp=input("Europe floor?")
+# usf=int(inp) +1
+# print('US floor', usf)
+
+# Пишемо коментар
+
+# hours = 35
+# pay = 2.75
+# earn = hours * pay
+# print("You earned", earn)
+
+# Домашня робота 2.3
+
+hrs = input("Enter Hours:")
+print(hrs)
+hrs=float(hrs)
+rate= input("Enter Rate:")
+print(rate)
+rate=float(rate)
+pay=hrs*rate
+print("Pay:", pay)
+
