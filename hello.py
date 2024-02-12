ca16b1ecc8bb37bff6ac95cb037014b4e47d56db
Vladimir Dyadenko <Stilmagroup@gmail.com>
2024-02-12 15:57:34 +0200

    topic 3

--- hello.py
+++ hello.py
@@ -14,12 +14,100 @@
 
 # Домашня робота 2.3
 
-hrs = input("Enter Hours:")
-print(hrs)
-hrs=float(hrs)
-rate= input("Enter Rate:")
-print(rate)
-rate=float(rate)
-pay=hrs*rate
-print("Pay:", pay)
+# hrs = input("Enter Hours:")
+# print(hrs)
+# hrs=float(hrs)
+# rate= input("Enter Rate:")
+# print(rate)
+# rate=float(rate)
+# pay=hrs*rate
+# print("Pay:", pay)
+
+# width = 17
+# height = 12.0
+# print(width//2)
+# print(width/2.0)
+# print(type(width))
+# print(height/32)
+# print(type(height))
+# print(1+2*5)
+
+# a=11
+# if a<10 :
+#     print("Less")
+# if a> 10 :
+#     print("More")
+
+# x=10
+# if x<2:
+#     print("Little")
+# elif x <10 :
+#     print("Середній")
+# else :
+#     print("Big")
+# print("successful")
+
+# astr = "Hello Bob"
+# try:
+#     astr = int(astr)
+# except:
+#     astr = -1
+# print("First", astr)
+
+# astr = "25"
+# try:
+#     astr = int(astr)
+# except:
+#     astr = -1
+# print("First", astr)
+
+# Execise 3.1
+# hrs = input("Enter Hours:")
+# try:
+#  h = float(hrs)   
+# except:
+#    print("Введіть число")
+#    quit()
+# rate = input("Enter Rate:")
+# try:
+#    rate= float(rate)   
+# except:
+#    print("Введіть число")
+#    quit()
+# rateUp = float(rate*1.5)
+# if h<= 40 :
+#     earn=h*rate
+#     print(earn)
+# else:
+#     earn = (rate*40) + (h-40)*rateUp
+#     print(earn)
+# print(type(h))
+# print(type(rate))
+# print(type(rateUp))
+
+# Execise 3.2
+# score = input("Enter Score: ")
+# try:
+#   score = float(score)
+# except:
+#     print("Bad score")
+#     quit()
+# if score > 1.0:
+#     print("Score mast be less 1.0")
+#     quit()
+# elif score >= 0.9 :
+#     res ="A"
+# elif score >=0.8 :
+#     res ="B"
+# elif score >= 0.7:
+#     res="C"
+# elif score >= 0.6:
+#     res="D"
+# elif score < 0.6:
+#     res="F"
+# print(res)
+
+
+
+    
 
